a2599cb90f44dc8d92893cf9a84ca4aa2198ab97
Lucas Souza <lucaseduardoid1@gmail.com>
2023-07-21 01:25:23 -0400

    feat: update game search view controller

--- Arcade/Controller/GameSearchViewController.swift
+++ Arcade/Controller/GameSearchViewController.swift
@@ -17,8 +17,13 @@ class GameSearchViewController: UIViewController {
     
     private var tableView: UITableView!
     private var searchController: UISearchController!
+    private var messageNotFound: UILabel!
+    private var messageSearchBy: UILabel!
+    private var labelsStatckView: UIStackView!
+    private var spinner: UIActivityIndicatorView!
     
     private var dataSource: DataSource?
+    private var searchTimer: Timer?
     
     private let gameService = GameService()
 
@@ -44,10 +49,35 @@ class GameSearchViewController: UIViewController {
         
         navigationItem.searchController = searchController
         navigationItem.hidesSearchBarWhenScrolling = false
+        
+        messageNotFound = UILabel()
+        messageNotFound.text = "No recent searches..."
+        messageNotFound.textColor = .black
+        messageNotFound.font = .boldSystemFont(ofSize: 20)
+        messageNotFound.numberOfLines = 0
+        messageNotFound.textAlignment = .center
+        
+        messageSearchBy = UILabel()
+        messageSearchBy.text = "Search by category."
+        messageSearchBy.textColor = .black
+        messageSearchBy.font = .systemFont(ofSize: 14)
+        
+        labelsStatckView = UIStackView()
+        labelsStatckView.axis = .vertical
+        labelsStatckView.alignment = .center
+        labelsStatckView.spacing = 6
+        
+        spinner = UIActivityIndicatorView(style: .large)
     }
     
     private func setupIU() {
         view.addSubview(tableView)
+        
+        labelsStatckView.addArrangedSubview(messageNotFound)
+        labelsStatckView.addArrangedSubview(messageSearchBy)
+        view.addSubview(labelsStatckView)
+        
+        view.addSubview(spinner)
     }
     
     private func setupConstraints() {
@@ -59,19 +89,55 @@ class GameSearchViewController: UIViewController {
             tableView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
             tableView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
         ])
+        
+        labelsStatckView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            labelsStatckView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+            labelsStatckView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            labelsStatckView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+        ])
+        
+        spinner.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            spinner.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+            spinner.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+        ])
     }
     
     private func loadFilteredGamesSnapshot(filter: String = "") async {
-        do {
-            let filteredGames = try await gameService.getGameByCategory(category: filter)
-            
-            var snapshot = Snapshot()
-            snapshot.appendSections([.main])
-            snapshot.appendItems(filteredGames)
+        spinner.startAnimating()
+        
+        var snapshot = Snapshot()
+        snapshot.appendSections([.main])
+        snapshot.appendItems([])
+        
+        guard !filter.isEmpty else {
             
             await dataSource?.apply(snapshot, animatingDifferences: true)
+            spinner.stopAnimating()
+            
+            labelsStatckView.isHidden = false
+            messageNotFound.text = "No recent searches..."
+            return
+        }
+        
+        do {
+            labelsStatckView.isHidden = true
+            
+            let filteredGames = try await gameService.getGameByCategory(category: filter)
+ 
+            snapshot.appendItems(filteredGames)
+            
+            spinner.stopAnimating()
+            await dataSource?.apply(snapshot, animatingDifferences: true)
         } catch {
-            //displayError(error, title: "No category found, please check the correct parameters")
+            await dataSource?.apply(snapshot, animatingDifferences: true)
+            spinner.stopAnimating()
+            
+            labelsStatckView.isHidden = false
+            messageNotFound.text = "No category found, please check the correct parameters..."
         }
     }
     
@@ -84,26 +150,20 @@ class GameSearchViewController: UIViewController {
             return cell
         }
     }
-    
-    func displayError(_ error: Error, title: String) {
-        guard let _ = viewIfLoaded?.window else { return }
-        
-        let alert = UIAlertController(title: title, message: error.localizedDescription, preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "Dismiss", style: .default, handler: nil))
-        
-        self.present(alert, animated: true, completion: nil)
-    }
 
 }
 
 extension GameSearchViewController: UISearchResultsUpdating {
     func updateSearchResults(for searchController: UISearchController) {
-        let searchString = searchController.searchBar.text
+        searchTimer?.invalidate()
         
-        let searchValue = searchString?.isEmpty ?? true ? "" : searchString?.lowercased()
+        guard let searchText = searchController.searchBar.text?.lowercased() else { return }
         
-        Task {
-            await self.loadFilteredGamesSnapshot(filter: searchValue!)
-        }
+        searchTimer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: false, block: { timer in
+            Task {
+                await self.loadFilteredGamesSnapshot(filter: searchText)
+            }
+        })
+                
     }
 }
